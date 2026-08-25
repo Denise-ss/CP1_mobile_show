042b2cee1217a84184a1922ddc5680d11fd73cc5
Fernando Pinéo <fernandopro1201@gmail.com>
2026-08-25 11:54:10 -0300

    Falta realiar a customização de fonte dos buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,56 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-        home: Container(
-          color: Colors.amber,
-          alignment: Alignment.center,
-          child: Text("Olá, Turma!")
-        )
-      );
+      home: HomePage()
+    );
   } 
 }
 
 class HomePage extends StatelessWidget{
-  const HomePage
+  //const HomePage(Key? key):super(key:key);
+  void decrement(){
+    print("Decrementando...");
+  }
+
+  void increment(){
+    print("Incrementando...");
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,//Alinhado no eixo da vertical
+        children: [
+          Text("Pode Entrar!",
+              style:
+                TextStyle(
+                  fontSize: 26,
+                  color:Colors.white,
+                  fontWeight: FontWeight.w700
+                )
+          ),
+          Text("0",
+            style: 
+              TextStyle(
+                fontSize: 100,
+                color:Colors.white
+              )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(onPressed: decrement,child:Text("Saiu")),
+              TextButton(onPressed: increment,child:Text("Entrou"))
+            ],
+          )
+        ],
+      )
+    );
+  }
 }
+
+
+
+
